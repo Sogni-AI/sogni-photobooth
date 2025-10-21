ff32633d5206c04687e04fedcb43316553fb7a85
Mark Ledford <markledford@gmail.com>
2025-10-21 16:07:12 -0700

    rearrange audio tracks

--- src/constants/musicPlaylist.js
+++ src/constants/musicPlaylist.js
@@ -4,24 +4,24 @@ export const PLAYLIST = [
     title: 'Bash Unstable'
   },
   {
-    url: 'https://pub-5bc58981af9f42659ff8ada57bfea92c.r2.dev/music/00_Slothi_in_the_booth.mp3',
-    title: 'Slothi in the Booth'
-  },
-  {
     url: 'https://pub-5bc58981af9f42659ff8ada57bfea92c.r2.dev/music/04_sogni_smash.mp3',
     title: 'Sogni Smash'
-  },
-  {
-    url: 'https://pub-5bc58981af9f42659ff8ada57bfea92c.r2.dev/music/01_spice_must_flow_(acapella).mp3',
-    title: 'Spice Must Flow (Acapella)'
   },
   {
     url: 'https://pub-5bc58981af9f42659ff8ada57bfea92c.r2.dev/music/02_can_i_get_a_render.mp3',
     title: 'Can I Get a Render?'
   },
   {
+    url: 'https://pub-5bc58981af9f42659ff8ada57bfea92c.r2.dev/music/01_spice_must_flow_(acapella).mp3',
+    title: 'Spice Must Flow (Acapella)'
+  },
+  {
     url: 'https://pub-5bc58981af9f42659ff8ada57bfea92c.r2.dev/music/03_Aint_No_Slop_in_My_Code.mp3',
     title: 'Power to Earn'
+  },
+  {
+    url: 'https://pub-5bc58981af9f42659ff8ada57bfea92c.r2.dev/music/00_Slothi_in_the_booth.mp3',
+    title: 'Slothi in the Booth'
   },
   {
     url: 'https://pub-5bc58981af9f42659ff8ada57bfea92c.r2.dev/music/05_we_spark_again.mp3',
